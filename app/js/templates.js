a515f6eb2bb97afa20785749a6fb6002a25fbd72
Vi Pham <tvpham11@gmail.com>
2015-06-16 10:22:38 -0400

    Added handlebars main template

--- app/js/templates.js
+++ app/js/templates.js
@@ -1,8 +1,12 @@
-this["template"] = this["template"] || {};
-this["template"]["example"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+this["hbs"] = this["hbs"] || {};
+this["hbs"]["example"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var helper;
 
   return "<h1>\n  "
     + this.escapeExpression(((helper = (helper = helpers.value || (depth0 != null ? depth0.value : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"value","hash":{},"data":data}) : helper)))
     + "\n</h1>\n";
+},"useData":true});
+this["hbs"] = this["hbs"] || {};
+this["hbs"]["main"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<form id=\"addBookmark\">\n  <input type=\"text\" placeholder=\"Bookmark Title\" id=\"bookmarkTitle\">\n  <input type=\"text\" placeholder=\"Bookmark URL\" id=\"bookmarkURL\">\n  <button>Add Bookmark</button>\n</form>\n";
 },"useData":true});
