1e7d8ccd31407b3acb00c739f84cfbbaa14e5db2
Vi Pham <tvpham11@gmail.com>
2015-06-16 15:20:32 -0400

    Added delete method

--- app/js/templates.js
+++ app/js/templates.js
@@ -32,11 +32,11 @@ this["hbs"] = this["hbs"] || {};
 this["hbs"]["single"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
     return "    <p>This has been visited</p>\n";
 },"3":function(depth0,helpers,partials,data) {
-    return "    <p>This has not been visited</p>\n";
+    return "    <p>This has NOT been visited</p>\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1, helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
-  return "<div class=\"bookmark\">\n\n  <h3>"
+  return "<div class=\"bookmark panel\">\n\n  <h3>"
     + alias3(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"title","hash":{},"data":data}) : helper)))
     + "</h3>\n  <p><a href=\""
     + alias3(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"url","hash":{},"data":data}) : helper)))
@@ -44,5 +44,7 @@ this["hbs"]["single"] = Handlebars.template({"1":function(depth0,helpers,partial
     + alias3(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"url","hash":{},"data":data}) : helper)))
     + "</a></p>\n\n"
     + ((stack1 = helpers['if'].call(depth0,(depth0 != null ? depth0.visited : depth0),{"name":"if","hash":{},"fn":this.program(1, data, 0),"inverse":this.program(3, data, 0),"data":data})) != null ? stack1 : "")
-    + "\n</div>\n";
+    + "\n  <button id=\"delete\" class=\"alert\" data-id=\""
+    + alias3(((helper = (helper = helpers._id || (depth0 != null ? depth0._id : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"_id","hash":{},"data":data}) : helper)))
+    + "\">Delete</button>\n\n</div>\n";
 },"useData":true});
--- app/js/views/single.view.js
+++ app/js/views/single.view.js
@@ -6,6 +6,10 @@
 
     template: hbs.single,
 
+    events: {
+      'click #delete': 'deleteBookmark'
+    },
+
     initialize: function(options) {
       var args = options || {};
       this.singleId = args.singleId;
@@ -18,7 +22,19 @@
     render: function() {
       var singleBookmark = this.collection.get(this.singleId);
       this.$el.html(this.template(singleBookmark.toJSON()));
-    }
+    },
 
+    deleteBookmark: function(event) {
+      var button = event.target;
+      var modelID = $(button).data('id');
+      var whichOne = this.collection.get(modelID);
+
+      whichOne.destroy().success(function(){
+        console.log('Item deleted');
+        // Message letting user know bookmark was deleted
+        // Force route - Navigate to home page
+      });
+    }
   });
+
 }());
